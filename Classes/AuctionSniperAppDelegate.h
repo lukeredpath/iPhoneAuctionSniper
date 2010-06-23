408b4a99185eb665baf2595bf11452730a3db1f3
Luke Redpath <luke@lukeredpath.co.uk>
2010-06-23 13:38:26 +0100

    Initial commit of blank window-template project.

---
+++ Classes/AuctionSniperAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AuctionSniperAppDelegate.h
+//  AuctionSniper
+//
+//  Created by Luke Redpath on 23/06/2010.
+//  Copyright LJR Software Limited 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AuctionSniperAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
