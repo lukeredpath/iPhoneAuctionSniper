9e7e04e8f5b96bb6e05fd256cc702352b219744a
Luke Redpath <luke@lukeredpath.co.uk>
2010-06-27 22:21:42 +0100

    Define a utility class and Cucumber helper for managing Openfire.
    We'll start Openfire before scenarios and close it after. If it fails
    to start, we raise an exception so we can fail fast.

--- Features/support/env.rb
+++ Features/support/env.rb
@@ -1,5 +1,48 @@
 require 'icuke/cucumber'
 
+OPENFIRE_PATH = "/opt/openfire/bin/openfire"
+
 ICukeWorld.configure do
   set :target, 'Testing'
+end
+
+module OpenfireHelpers
+  class Openfire
+    def initialize(path)
+      @path = path
+    end
+
+    def start
+      run "start"
+    end
+
+    def stop
+      run "stop"
+    end
+
+    def running?
+      run "status" # returns > 0 if not running
+    end
+
+    private
+
+    def run(command)
+      system("#{@path} #{command} > /dev/null")
+    end
+  end
+  
+  def openfire
+    @openfire ||= Openfire.new(OPENFIRE_PATH)
+  end
+end
+
+World(OpenfireHelpers)
+
+Before do
+  openfire.start
+  raise "Could not start Openfire" unless openfire.running?
+end
+
+After do
+  openfire.stop
 end
