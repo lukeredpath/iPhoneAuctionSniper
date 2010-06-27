7d275d365bdf35c036542e0c7408cbce38ff3db2
Luke Redpath <luke@lukeredpath.co.uk>
2010-06-28 00:53:07 +0100

    Made some changes to the cucumber environment.
    We should only start and stop the openfire server at the beginning and end of
    the entire run. We also need to wait a bit to give the server some time to start up.

--- Features/support/env.rb
+++ Features/support/env.rb
@@ -1,5 +1,7 @@
 require 'icuke/cucumber'
-$LOAD_PATH << File.join(File.dirname(__FILE__), *%w[.. support])
+require 'test/unit/assertions'
+
+World(Test::Unit::Assertions)
 
 OPENFIRE_PATH = "/opt/openfire/bin/openfire"
 
@@ -7,43 +9,38 @@ ICukeWorld.configure do
   set :target, 'Testing'
 end
 
-module OpenfireHelpers
-  class Openfire
-    def initialize(path)
-      @path = path
-    end
-
-    def start
-      run "start"
-    end
-
-    def stop
-      run "stop"
-    end
-
-    def running?
-      run "status" # returns > 0 if not running
-    end
-
-    private
-
-    def run(command)
-      system("#{@path} #{command} > /dev/null")
-    end
+class Openfire
+  def initialize(path)
+    @path = path
   end
-  
-  def openfire
-    @openfire ||= Openfire.new(OPENFIRE_PATH)
+
+  def start
+    run "start"
+  end
+
+  def stop
+    run "stop"
+  end
+
+  def running?
+    run "status" # returns > 0 if not running
+  end
+
+  private
+
+  def run(command)
+    system("#{@path} #{command} > /dev/null")
   end
 end
 
-World(OpenfireHelpers)
+$openfire ||= Openfire.new(OPENFIRE_PATH)
 
-Before do
-  openfire.start
-  raise "Could not start Openfire" unless openfire.running?
+AfterConfiguration do
+  $openfire.start
+  raise "Could not start Openfire" unless $openfire.running?
+  sleep(4) # give the daemon a chance to start up
 end
 
-After do
-  openfire.stop
+at_exit do
+  $openfire.stop
 end
