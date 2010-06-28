1a7e6fa4adb497e2fd87d41fd3cf4389525e6111
Luke Redpath <luke@lukeredpath.co.uk>
2010-06-28 01:42:23 +0100

    First step towards a passing spec; add a static label to the screen containing a "Joining" (the default) status.
    We've made one design leap here, and that is to add a view controller rather than add all
    of this to the main window.

---
+++ Classes/AuctionSniperViewController.h
@@ -0,0 +1,16 @@
+//
+//  AuctionSniperViewController.h
+//  AuctionSniper
+//
+//  Created by Luke Redpath on 28/06/2010.
+//  Copyright 2010 LJR Software Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AuctionSniperViewController : UIViewController {
+
+}
+
+@end
---
+++ Classes/AuctionSniperViewController.m
@@ -0,0 +1,31 @@
+//
+//  AuctionSniperViewController.m
+//  AuctionSniper
+//
+//  Created by Luke Redpath on 28/06/2010.
+//  Copyright 2010 LJR Software Limited. All rights reserved.
+//
+
+#import "AuctionSniperViewController.h"
+
+
+@implementation AuctionSniperViewController
+
+- (void)dealloc 
+{
+  [super dealloc];
+}
+
+- (void)viewDidLoad 
+{
+  [super viewDidLoad];
+  
+  
+}
+
+- (void)viewDidUnload 
+{
+  [super viewDidUnload];
+}
+
+@end
