a0518b092d706688fc88ab845ed54e3fb0aefa6f
Luke Redpath <luke@lukeredpath.co.uk>
2010-06-29 01:57:28 +0100

    Fixed a typo.

--- Classes/AuctionSniperAppDelegate.m
+++ Classes/AuctionSniperAppDelegate.m
@@ -98,7 +98,7 @@ XMPPJID *auctionJID() {
   AuctionSniper *auctionSniper = [[AuctionSniper alloc] initWithAuction:auction];
   self.auctionSniperController.auctionSniper = auctionSniper;
   
-  messageTranslator = [[AuctionMessageTranslator alloc] initWithSniperID:xmppStream.myJID.base eventListener:auctionSniper];
+  messageTranslator = [[AuctionMessageTranslator alloc] initWithSniperID:xmppStream.myJID.bare eventListener:auctionSniper];
   [xmppStream addDelegate:messageTranslator];
   
   [auction release];
